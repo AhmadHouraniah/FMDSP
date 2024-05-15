f5dfe8c959fc41d6970aafa81e9115a5aff5dba8
ahmad houraniah <ahmad.houraniah@ozu.edu.tr>
2024-05-16 00:58:09 +0300

    updated dir

--- FB32DSP/sim.sh
+++ FB32DSP/sim.sh
@@ -1,1 +1,1 @@
-iverilog -o out.run tb.v DSP_model.v mcmult2o.v mult_wrapper.v compressor.v cells/flop.v cells/ha.v cells/fa.v dadda.v wallace.v ; vvp out.run
+iverilog -o out.run tb.v DSP_model.v mcmult2o.v ../cells/mult_wrapper.v ../cells/compressor.v ../cells/flop.v ../cells/ha.v ../cells/fa.v ../cells/dadda.v ../cells/wallace.v ; vvp out.run
