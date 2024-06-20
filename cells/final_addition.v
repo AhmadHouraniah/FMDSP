ab5a993e625d165c76d2a0488af1bbd748fad35e
ahmad houraniah <ahmad.houraniah@ozu.edu.tr>
2024-06-20 19:06:17 +0300

    added sr to final_addition

--- cells/final_addition.v
+++ cells/final_addition.v
@@ -17,12 +17,20 @@ module final_addition
     
     assign carry[0] = 1'b0;
 
+    wire [WIDTH-1:0] in1_sr [WIDTH/PIPE_STAGE_WIDTH  :0];
+    wire [WIDTH-1:0] in2_sr [WIDTH/PIPE_STAGE_WIDTH  :0];
+    
+    assign in1_sr[0] = in1;
+    assign in2_sr[0] = in2;
+
     generate
         genvar ii;
         for(ii = 0; ii < WIDTH; ii = ii + PIPE_STAGE_WIDTH) begin : ADD_STAGE
-            final_addition_stage #(.WIDTH(PIPE_STAGE_WIDTH)) stage (
-                .in1(in1[ii +: PIPE_STAGE_WIDTH]), 
-                .in2(in2[ii +: PIPE_STAGE_WIDTH]), 
+            final_addition_stage #(.WIDTH(PIPE_STAGE_WIDTH), .SR_WIDTH(WIDTH)) stage (
+                .in1(in1_sr[ii/PIPE_STAGE_WIDTH]), 
+                .in2(in2_sr[ii/PIPE_STAGE_WIDTH]), 
+                .in1_sr(in1_sr[1+ii/PIPE_STAGE_WIDTH]), 
+                .in2_sr(in2_sr[1+ii/PIPE_STAGE_WIDTH]),
                 .carry_in(carry[ii / PIPE_STAGE_WIDTH]), 
                 .out(out[ii +: PIPE_STAGE_WIDTH]),
                 .carry_out(carry[(ii / PIPE_STAGE_WIDTH) + 1]), 
@@ -52,16 +60,19 @@ endmodule
 
 module final_addition_stage
 #(
+    parameter SR_WIDTH = 16,
     parameter WIDTH = 2
 )
 (
-    input [WIDTH-1:0] in1,
-    input [WIDTH-1:0] in2,
+    input [SR_WIDTH-1:0] in1,
+    input [SR_WIDTH-1:0] in2,
     input clk,
     input pipelined,
     input carry_in,
     output carry_out,
-    output [WIDTH-1:0] out
+    output [WIDTH-1:0] out,
+    output [SR_WIDTH -1 :0] in1_sr,
+    output [SR_WIDTH -1 :0] in2_sr    
 );
 
     wire [WIDTH:0] carry;
@@ -95,125 +106,146 @@ module final_addition_stage
         .out(c_o_r)
     );
 
+    wire [SR_WIDTH-WIDTH -1 :0] in1_sr_reg;
+
+    flop #(SR_WIDTH-WIDTH ) flop_in1_sr (
+        .in(in1[SR_WIDTH -1 :WIDTH]), 
+        .clk(clk), 
+        .out(in1_sr_reg)
+    );
+
+    wire [SR_WIDTH-WIDTH -1 :0] in2_sr_reg;
+    
+    flop #(SR_WIDTH-WIDTH ) flop_in2_sr (
+        .in(in2[SR_WIDTH -1 :WIDTH]), 
+        .clk(clk), 
+        .out(in2_sr_reg)
+    );
+
+    assign in1_sr[SR_WIDTH-WIDTH -1 :0]  = pipelined ? in1_sr_reg : in1[SR_WIDTH :WIDTH];
+    assign in2_sr[SR_WIDTH-WIDTH -1 :0]  = pipelined ? in2_sr_reg : in2[SR_WIDTH -1 :WIDTH];
+    assign in1_sr[SR_WIDTH -1 :SR_WIDTH-WIDTH] = 0;
+    assign in2_sr[SR_WIDTH -1 :SR_WIDTH-WIDTH] = 0;
+    
     assign out = pipelined ? sum_r : sum;
     assign carry_out = pipelined ? c_o_r : carry[WIDTH];
 
 endmodule
 
 
-
-module tb_final_addition;
-
-    parameter WIDTH = 16;
-    parameter PIPELINE_BITS = 3;
-
-    // Inputs
-    reg [WIDTH-1:0] in1;
-    reg [WIDTH-1:0] in2;
-    reg clk;
-    reg [PIPELINE_BITS-1:0] pipes;
-
-    // Outputs
-    wire [WIDTH-1:0] out;
-
-    // Instantiate the Unit Under Test (UUT)
-    final_addition #(
-        .WIDTH(WIDTH),
-        .PIPELINE_BITS(PIPELINE_BITS)
-    ) uut (
-        .in1(in1),
-        .in2(in2),
-        .clk(clk),
-        .pipes(pipes),
-        .out(out)
-    );
-
-    // Clock generation
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk; // 100MHz clock
-    end
-    integer ii;
-    // Stimulus
-    initial begin
-        $dumpvars;
-        // Initialize Inputs
-        in1 = 0;
-        in2 = 0;
-        pipes = 0;
-
-        // Wait for the global reset
-        #11;
-
-        // Apply test vectors
-        // Test case 1
-        pipes = 0;
-        in1 = 1;
-        in2 = 2;
-        #1;
-        if(in1+in2 == out)
-            $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
-        else
-            $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
-        #100;
-
-        // Test case 2
-        pipes = 1;
-        in1 = 2500;
-        in2 = 7850;
-        #20;
-        if(in1+in2 == out)
-            $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
-        else
-            $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);        #100;
-
-
-        // Test case 3
-        pipes = 2;
-        in1 = 789;
-        in2 = 4562;
-        #40;
-        if(in1+in2 == out)
-            $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
-        else
-            $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);        #100;
-
-        // Test case 4
-        pipes = 3;
-        in1 = 4562;
-        in2 = 4integer ii;544;
-        #80;
-        if(in1+in2 == out)
-            $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
-        else
-            $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);        // Finish simulation
-        #100;
-
-
-
-        pipes = 4;
-        in1 = 4562;
-        in2 = 4544;
-        #160;
-        if(in1+in2 == out)
-            $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
-        else
-            $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);        // Finish simulation
-        #100;
-
-        
-        for(ii=0; ii<50; ii= ii+1) begin
-            pipes = 4;
-            in1 = $random;
-            in2 = $random;
-            #160;
-            if(in1+in2 == out)
-                $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
-            else
-                $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);        // Finish simulation
-        end
-        #100;
-        $finish;
-    end
-
-endmodule
+//
+//module tb_final_addition;
+//
+//    parameter WIDTH = 16;
+//    parameter PIPELINE_BITS = 3;
+//
+//    // Inputs
+//    reg [WIDTH-1:0] in1;
+//    reg [WIDTH-1:0] in2;
+//    reg clk;
+//    reg [PIPELINE_BITS-1:0] pipes;
+//
+//    // Outputs
+//    wire [WIDTH-1:0] out;
+//
+//    // Instantiate the Unit Under Test (UUT)
+//    final_addition #(
+//        .WIDTH(WIDTH),
+//        .PIPELINE_BITS(PIPELINE_BITS)
+//    ) uut (
+//        .in1(in1),
+//        .in2(in2),
+//        .clk(clk),
+//        .pipes(pipes),
+//        .out(out)
+//    );
+//
+//    // Clock generation
+//    initial begin
+//        clk = 0;
+//        forever #5 clk = ~clk; // 100MHz clock
+//    end
+//    integer ii;
+//    // Stimulus
+//    initial begin
+//        $dumpvars;
+//        // Initialize Inputs
+//        in1 = 0;
+//        in2 = 0;
+//        pipes = 0;
+//
+//        // Wait for the global reset
+//        #11;
+//
+//        // Apply test vectors
+//        // Test case 1
+//        pipes = 0;
+//        in1 = 1;
+//        in2 = 2;
+//        #1;
+//        if(in1+in2 == out)
+//            $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
+//        else
+//            $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
+//        #100;
+//
+//        // Test case 2
+//        pipes = 1;
+//        in1 = 2500;
+//        in2 = 7850;
+//        #20;
+//        if(in1+in2 == out)
+//            $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
+//        else
+//            $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);        #100;
+//
+//
+//        // Test case 3
+//        pipes = 2;
+//        in1 = 789;
+//        in2 = 4562;
+//        #40;
+//        if(in1+in2 == out)
+//            $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
+//        else
+//            $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);        #100;
+//
+//        // Test case 4
+//        pipes = 3;
+//        in1 = 4562;
+//        in2 = 4544;
+//        #80;
+//        if(in1+in2 == out)
+//            $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
+//        else
+//            $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);        // Finish simulation
+//        #100;
+//
+//
+//
+//        pipes = 4;
+//        in1 = 4562;
+//        in2 = 4544;
+//        #160;
+//        if(in1+in2 == out)
+//            $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
+//        else
+//            $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);        // Finish simulation
+//        #100;
+//
+//        
+//        for(ii=0; ii<50; ii= ii+1) begin
+//            pipes = $urandom_range(0, 3);
+//            in1 = $random;
+//            in2 = $random;
+//            #(pipes*10);
+//            if(in1+in2 == out)
+//                $display("Correct! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);
+//            else
+//                $display("Error! Test Case 1: in1 = %d, in2 = %d, pipes = %d, out = %d", in1, in2, pipes, out);        // Finish simulation
+//        end
+//        #100;
+//        $finish;
+//    end
+//
+//endmodule
