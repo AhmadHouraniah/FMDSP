af8fa76ab759868d15cfb25cd726d15da78650bd
ahmad houraniah <ahmad.houraniah@ozu.edu.tr>
2024-06-26 19:08:01 +0300

    openlane paths

---
+++ paths.sh
@@ -0,0 +1,5 @@
+export PDK_ROOT=/home/ahmad/caravel_user_project/dependencies/pdks/
+export CARAVEL_ROOT=/home/ahmad/caravel_user_project/caravel/
+export OPENLANE_ROOT=/home/ahmad/caravel_user_project/dependencies/openlane_src/
+export PDK=sky130A
+
