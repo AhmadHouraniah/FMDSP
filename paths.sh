548431dbf99e05e9bb55136f1f0bedc5cec6b085
ahmad houraniah <ahmad.houraniah@ozu.edu.tr>
2024-08-27 18:31:01 +0300

    updated Makefile

--- paths.sh
+++
@@ -1,5 +0,0 @@
-export PDK_ROOT=/home/ahmad/caravel_user_project/dependencies/pdks/
-export CARAVEL_ROOT=/home/ahmad/caravel_user_project/caravel/
-export OPENLANE_ROOT=/home/ahmad/caravel_user_project/dependencies/openlane_src/
-export PDK=sky130A
-
